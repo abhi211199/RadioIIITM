df3d0b5b2aee0597eaca4256b6df73dd462998bd
Abhishek Garain <abhi211199@gmail.com>
2018-12-10 20:52:39 +0530

    added all files, (navbar auto, music sync, pages pending)

---
+++ radiojs.js
@@ -0,0 +1,70 @@
+function revealAboutUs()
+{
+	document.getElementById("AboutUs").style.display = 'block';
+	document.getElementById("newsfeed").style.display = 'none';
+	document.getElementById("contact").style.display = 'none';
+	document.getElementById("radioIIITM").style.display = 'none';
+	document.getElementById("foot").style.position = '';
+	document.getElementById("foot").style.width = '100%';
+	document.getElementById("abt").style.backgroundColor = 'black';
+	document.getElementById("abt").style.borderRadius = '5px';
+	document.getElementById("contact1").style.backgroundColor = 'transparent';
+	document.getElementById("contact1").style.borderRadius = '0px';
+	document.getElementById("home").style.backgroundColor = 'transparent';
+	document.getElementById("home").style.borderRadius = '0px';
+	document.getElementById("fm").style.backgroundColor = 'transparent';
+	document.getElementById("fm").style.borderRadius = '0px';
+}
+function revealContactUs()
+{
+	document.getElementById("AboutUs").style.display = 'none';
+	document.getElementById("newsfeed").style.display = 'none';
+	document.getElementById("contact").style.display = 'block';
+	document.getElementById("radioIIITM").style.display = 'none';
+	document.getElementById("foot").style.position = 'fixed';
+	document.getElementById("foot").style.width = '100%';
+	document.getElementById("contact1").style.backgroundColor = 'black';
+	document.getElementById("contact1").style.borderRadius = '5px';
+	document.getElementById("abt").style.backgroundColor = 'transparent';
+	document.getElementById("abt").style.borderRadius = '0px';
+	document.getElementById("home").style.backgroundColor = 'transparent';
+	document.getElementById("home").style.borderRadius = '0px';
+	document.getElementById("fm").style.backgroundColor = 'transparent';
+	document.getElementById("fm").style.borderRadius = '0px';
+}
+
+function revealHome() 
+{
+	document.getElementById("AboutUs").style.display = 'none';
+	document.getElementById("newsfeed").style.display = 'block';
+	document.getElementById("contact").style.display = 'none';
+	document.getElementById("radioIIITM").style.display = 'none';
+	document.getElementById("foot").style.position = '';
+	document.getElementById("foot").style.width = '100%';
+	document.getElementById("home").style.backgroundColor = 'black';
+	document.getElementById("home").style.borderRadius = '5px';
+	document.getElementById("abt").style.backgroundColor = 'transparent';
+	document.getElementById("abt").style.borderRadius = '0px';
+	document.getElementById("contact1").style.backgroundColor = 'transparent';
+	document.getElementById("contact1").style.borderRadius = '0px';
+	document.getElementById("fm").style.backgroundColor = 'transparent';
+	document.getElementById("fm").style.borderRadius = '0px';
+}
+
+function revealRadio() 
+{
+	document.getElementById("AboutUs").style.display = 'none';
+	document.getElementById("newsfeed").style.display = 'none';
+	document.getElementById("contact").style.display = 'none';
+	document.getElementById("radioIIITM").style.display = 'block';
+	document.getElementById("foot").style.position = 'fixed';
+	document.getElementById("foot").style.width = '100%';
+	document.getElementById("fm").style.backgroundColor = 'black';
+	document.getElementById("fm").style.borderRadius = '5px';
+	document.getElementById("abt").style.backgroundColor = 'transparent';
+	document.getElementById("abt").style.borderRadius = '0px';
+	document.getElementById("contact1").style.backgroundColor = 'transparent';
+	document.getElementById("contact1").style.borderRadius = '0px';
+	document.getElementById("home").style.backgroundColor = 'transparent';
+	document.getElementById("home").style.borderRadius = '0px';
+}
